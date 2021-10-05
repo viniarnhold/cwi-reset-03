e7a9031600bf7e70b25302e6d37dc13122144c44
viniarnhold <viniciusarnh@gmail.com>
2021-10-05 13:35:51 -0300

    Adicionado regras de reposição de torta inteira e mudança na reposição dos demais itens

--- desafio-aula-1/registradora-cheia-de-bugs/src/ReposicaoCozinha.java
+++ desafio-aula-1/registradora-cheia-de-bugs/src/ReposicaoCozinha.java
@@ -1,14 +1,17 @@
 public class ReposicaoCozinha {
 
     static void reporItem(String item) {
+        //Método adiciona a quantidade do item em questão ao "estoque"
+
         if ("paes".equals(item)) {
-            ItensPorQuantidade.pao = 3600;
+            ItensPorQuantidade.pao += 3600;
         }
         if ("torta".equals(item)) {
-            ItensPorQuantidade.torta = 4;
+            int tortaInteira = 4;
+            ItensPorQuantidade.tortaInteira += 4;
         }
         if ("sanduiche".equals(item)) {
-            ItensPorQuantidade.sanduiche = 20;
+            ItensPorQuantidade.sanduiche += 20;
         }
     }
 }
--- desafio-aula-1/registradora-cheia-de-bugs/src/ReposicaoFornecedor.java
+++ desafio-aula-1/registradora-cheia-de-bugs/src/ReposicaoFornecedor.java
@@ -3,14 +3,15 @@ import java.util.Random;
 public class ReposicaoFornecedor {
 
     static void reporItem(String item) {
+        //Método adiciona a quantidade do item em questão ao "estoque", quando é um dia útil
         Random random = new Random();
 
         if ("leite".equals(item)) {
-            ItensPorQuantidade.leite = random.nextInt(40) + 10;
+            ItensPorQuantidade.leite += random.nextInt(40) + 10;
         }
 
         if ("cafe".equals(item)) {
-            ItensPorQuantidade.cafe = random.nextInt(40) + 10;
+            ItensPorQuantidade.cafe += random.nextInt(40) + 10;
         }
     }
 }
