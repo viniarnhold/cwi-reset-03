c62e0b1f39576268fe19d20f1ab289edc24376ef
viniarnhold <viniciusarnh@gmail.com>
2021-10-18 13:50:59 -0300

    Adicionado ResponseStatus às exceptions e mudança nos resources, para funcionamento das respostas de exceptions na aplicação web

--- desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/viniciusarnhold/HelloWorld.java
+++ desafio-resetflix/api-resetflix/src/main/java/br/com/cwi/reset/viniciusarnhold/HelloWorld.java
@@ -1,5 +1,6 @@
 package br.com.cwi.reset.viniciusarnhold;
 
+import br.com.cwi.reset.viniciusarnhold.exceptions.AtorAnoInicioAtividadeException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorld {
 
     @GetMapping
-    public String helloWorld(){
-        return "Hello World";
+    public String helloWorld() throws AtorAnoInicioAtividadeException {
+        throw new AtorAnoInicioAtividadeException();
+        //return "Hello World";
     }
 }
